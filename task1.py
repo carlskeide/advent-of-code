dc5ca1e1d9a6793a59d65e815f0b344b59a711c2
Carl Skeide <carl.skeide@sony.com>
2020-12-01 16:21:00 +0100

    Task 1 complete

--- task1.py
+++ task1.py
@@ -3,25 +3,15 @@
 # find the two entries that sum to 2020 and then multiply those two numbers together.
 # https://adventofcode.com/2020/day/1/input
 
-
-def part1(expenses):
-    for item in expenses:
-        needle = abs(item - 2020)
-        if needle in expenses:
-            print(f"{item} + {needle} = 2020")
-            return item * needle
-
-    else:
-        raise ValueError("No Match!")
+from itertools import combinations
+from math import prod
 
 
-def part2(expenses):
-    for item in expenses:
-        for item2 in expenses:
-            for item3 in expenses:
-                if sum((item, item2, item3)) == 2020:
-                    print(f"{item} * {item2} * {item3} = 2020")
-                    return item * item2 * item3
+def task1(expenses, num_items):
+    for items in combinations(expenses, num_items):
+        if sum(items) == 2020:
+            print(f"{items} = 2020")
+            return prod(items)
 
     else:
         raise ValueError("No Match!")
@@ -29,7 +19,7 @@ def part2(expenses):
 
 if __name__ == "__main__":
     with open("./task1.input") as f:
-        expenses = [int(r) for r in f.readlines()]
+        expenses = {int(r) for r in f.readlines()}
 
-    print("Part 1: {}".format(part1(expenses)))
-    print("Part 2: {}".format(part2(expenses)))
+    print("Part 1: {}".format(task1(expenses, 2)))
+    print("Part 2: {}".format(task1(expenses, 3)))
