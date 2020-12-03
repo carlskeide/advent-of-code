62d3903f9480bf6d8a26e46ac5964796ad523995
Carl Skeide <carl.skeide@sony.com>
2020-12-03 13:21:49 +0100

    Task 3, minial approach

--- task3.py
+++ task3.py
@@ -8,31 +8,21 @@ from math import prod
 
 
 def toboggan(terrain, delta_x, delta_y):
-    x, y = 0, 0
-
-    while True:
-        try:
-            row = terrain[y]
-            yield row[x % len(row)] == "#"
-
-            x += delta_x
-            y += delta_y
-
-        except IndexError:
-            return
+    col = 0
+    for row in terrain[::delta_y]:
+        yield row[col % len(row)] == "#"
+        col += delta_x
 
 
 if __name__ == "__main__":
     with open("./task3.input") as f:
         terrain = [line.strip() for line in f.readlines() if line]
 
-    steps = list(toboggan(terrain, delta_x=3, delta_y=1))
-    print(f"Part 1: hit {sum(steps)} trees out of {len(steps)} steps")
+    print(f"Part 1: hit {sum(toboggan(terrain, delta_x=3, delta_y=1))} trees")
 
-    print("Part 2:")
     trees_hit = []
     for delta_x, delta_y in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
         trees_hit.append(sum(toboggan(terrain, delta_x, delta_y)))
         print(f"Vector {delta_x}, {delta_y}: hit {trees_hit[-1]} trees")
 
-    print(f"Result: {prod(trees_hit)}")
+    print(f"Path 2: result {prod(trees_hit)}")
