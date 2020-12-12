a8d7a5b315afbaa0275b16595e7f7f5dd0a4e593
Carl Skeide <carl@skeide.se>
2020-12-12 12:08:03 +0100

    Task 12, slightly over-engineered

---
+++ src/test_task12.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+from unittest import TestCase
+
+from .task12 import Navigator, WaypointNavigator
+
+
+class TestNavigator(TestCase):
+    navigator_class = Navigator
+
+    instructions = [
+        "F10",
+        "N3",
+        "F7",
+        "R90",
+        "F11"
+    ]
+
+    def setUp(self):
+        self.navigator = self.navigator_class(face="E")
+
+    def test_navigate(self):
+        self.navigator.follow(self.instructions)
+
+        self.assertEqual(self.navigator.face, "S")
+        self.assertEqual(self.navigator.position, {"x": 17, "y": -8})
+        self.assertEqual(self.navigator.distance, 25)
+
+
+class TestWaypointNavigator(TestNavigator):
+    navigator_class = WaypointNavigator
+
+    def test_navigate(self):
+        self.navigator.follow(self.instructions)
+
+        self.assertEqual(self.navigator.position, {"x": 214, "y": -72})
+        self.assertEqual(self.navigator.distance, 286)
