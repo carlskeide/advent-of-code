e67cb2c6f747753a8e25ded30266dd58561439b2
Carl Skeide <carl@skeide.se>
2020-12-22 14:37:55 +0100

    Task 20, part 2

--- tests/test_task20.py
+++ tests/test_task20.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 from unittest import TestCase
 
-from src.task20 import parse_tile
+from src.task20 import Tile
 
 
 class TestTiles(TestCase):
-    def test_parse_tile(self):
-        tile = (
+    def setUp(self):
+        self.tile = Tile(
             "Tile 2311:\n"
             "..##.#..#.\n"
             "##..#.....\n"
@@ -20,7 +20,65 @@ class TestTiles(TestCase):
             "..###..###"
         )
 
-        tile_id, edges = parse_tile(tile)
-        self.assertEqual(tile_id, "2311")
-        self.assertListEqual(
-            ["..##.#..#.", "...#.##..#", "..###..###", ".#####..#."], edges)
+    def test_attr(self):
+        self.assertEqual(self.tile.id, 2311)
+
+        self.assertDictEqual({
+            "N": "..##.#..#.",
+            "E": "...#.##..#",
+            "S": "..###..###",
+            "W": ".#####..#."
+        }, self.tile.edges)
+
+        self.assertListEqual([
+            "#..#....",
+            "...##..#",
+            "###.#...",
+            "#.##.###",
+            "#...#.##",
+            "#.#.#..#",
+            ".#....#.",
+            "##...#.#"
+        ], self.tile.area)
+
+    def test_rotate(self):
+        self.tile.rotate()
+        self.assertDictEqual({
+            'E': '..##.#..#.',
+            'N': '.#..#####.',
+            'S': '#..##.#...',
+            'W': '..###..###'
+        }, self.tile.edges)
+
+        self.assertListEqual([
+            '#.####.#',
+            '##...#..',
+            '..#.##..',
+            '....#.##',
+            '..##.##.',
+            '#...#...',
+            '.#.##...',
+            '#.###.#.'
+        ], self.tile.area)
+
+    def test_flip(self):
+        self.tile.flip()
+
+        self.assertDictEqual({
+            "N": "..###..###",
+            "E": "#..##.#...",
+            "S": "..##.#..#.",
+            "W": ".#..#####."
+        }, self.tile.edges)
+
+        print(self.tile.area)
+        self.assertListEqual([
+            '##...#.#',
+            '.#....#.',
+            '#.#.#..#',
+            '#...#.##',
+            '#.##.###',
+            '###.#...',
+            '...##..#',
+            '#..#....'
+        ], self.tile.area)
