ef70fb00d814998aafaa930a44c623708e2daa24
Carl Skeide <carl@skeide.se>
2020-12-22 17:45:33 +0100

    Task 22, part 2 (?)

--- src/task22.py
+++ src/task22.py
@@ -23,14 +23,50 @@ def score(deck):
     )
 
 
+def recursive_combat(deck1, deck2, depth=1):
+    cache = []
+
+    print(f"starting game #{depth}")
+    while deck1 and deck2:
+        signature = (tuple(deck1), tuple(deck2))
+        if signature in cache:
+            print("infinite loop!")
+            return (deck1, [])
+
+        else:
+            cache.append(signature)
+
+        card1 = deck1.pop(0)
+        card2 = deck2.pop(0)
+        # print(f"{card1} vs {card2}")
+
+        if len(deck1) >= card1 and len(deck2) >= card2:
+            print("subgame")
+            result = recursive_combat(deck1[:], deck2[:], depth=depth + 1)
+            if result[0]:
+                deck1.extend((card1, card2))
+
+            else:
+                deck2.extend((card2, card1))
+
+        elif card1 > card2:
+            deck1.extend((card1, card2))
+
+        else:
+            deck2.extend((card2, card1))
+
+    print(f"ending game #{depth}")
+    return (deck1, deck2)
+
+
 if __name__ == "__main__":
     task_input = load_input(day=22, group_lines=True)
-    decks = [
+    deck1, deck2 = [
         [int(card) for card in deck.splitlines()[1:]] for deck in task_input
     ]
 
-    result = combat(*decks)
-    print(f"Part 1: {score(result)}")
+    # result = combat(deck1[:], deck2[:])
+    # print(f"Part 1: {score(result)}")
 
-    part2 = ""
-    print(f"Part 2: {part2}")
+    result = recursive_combat(deck1, deck2)
+    print(f"Part 2: {result}")
--- tests/test_task22.py
+++ tests/test_task22.py
@@ -1,15 +1,23 @@
 # coding=utf-8
 from unittest import TestCase
 
-from src.task22 import combat, score
+from src.task22 import combat, recursive_combat, score
 
 
 class TestTask(TestCase):
+    def setUp(self):
+        self.decks = [
+            [9, 2, 6, 3, 1],
+            [5, 8, 4, 7, 10]
+        ]
+
     def test_combat(self):
-        deck1 = [9, 2, 6, 3, 1]
-        deck2 = [5, 8, 4, 7, 10]
-        result = combat(deck1, deck2)
+        result = combat(self.decks[0], self.decks[1])
         self.assertListEqual(result, [3, 2, 10, 6, 8, 5, 9, 4, 7, 1])
+
+    def test_recursive_combat(self):
+        result = recursive_combat(self.decks[0], self.decks[1])
+        self.assertTupleEqual(result, ([], [7, 5, 6, 2, 4, 1, 10, 8, 9, 3]))
 
     def test_score(self):
         result = score([3, 2, 10, 6, 8, 5, 9, 4, 7, 1])
