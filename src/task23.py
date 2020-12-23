d6d1b3b7abc31b6a87ce48fbc326f7bb832cc1c1
Carl Skeide <carl@skeide.se>
2020-12-23 15:30:35 +0100

    Task 23, part 2

--- src/task23.py
+++ src/task23.py
@@ -2,36 +2,71 @@
 from . import load_input
 
 
-def move_cups(cups):
-    first, *other = cups
-    moved = [other.pop(0) for _ in range(3)]
-    target = first - 1
-    while target not in other:
-        target = target - 1 if target > min(cups) else max(cups)
-
-    ix = other.index(target) + 1
-    return other[:ix] + moved + other[ix:] + [first]
+class Cup:
+    def __init__(self, label):
+        self.label = label
+        self.next = None
 
 
-def cformat(cups):
-    ix = cups.index(1)
-    return "".join(str(c) for c in cups[ix + 1:] + cups[:ix])
+class Ring:
+    def __init__(self, labels):
+        self.min = min(labels)
+        self.max = max(labels)
+
+        self.cups = {}
+        prev = None
+        for n in labels:
+            cup = Cup(n)
+            if prev:
+                prev.next = cup
+
+            else:
+                self.active = cup
+
+            self.cups[n] = prev = cup
+
+        cup.next = self.active
+
+    def subcups(self, target, count):
+        cur = self.cups[target].next
+        for _ in range(count):
+            yield cur.label
+            cur = cur.next
+
+    def move(self):
+        first = cur = self.active
+
+        moved = []
+        for _ in range(3):
+            cur = cur.next
+            moved.append(cur)
+
+        first.next = self.active = cur.next
+
+        invalid_targets = [cup.label for cup in moved]
+        target = first.label - 1
+        while target in invalid_targets or target < 1:
+            target = target - 1 if target > self.min else self.max
+
+        target_cup = self.cups[target]
+        target_cup.next, moved[-1].next = moved[0], target_cup.next
 
 
 if __name__ == "__main__":
     task_input = load_input(day=23)
     cups = [int(n) for n in task_input[0]]
+    ring = Ring(cups)
 
     for _ in range(100):
-        cups = move_cups(cups)
+        ring.move()
 
-    print(f"Part 1: {cformat(cups)}")
+    print(f"Part 1: {''.join(str(c) for c in ring.subcups(1, 8))}")
 
     many_cups = [int(n) for n in task_input[0]]
     many_cups += list(range(max(many_cups) + 1, 1000001))
+    ring = Ring(many_cups)
+    for _ in range(10000000):
+        ring.move()
 
-    for _ in range(1000):
-        many_cups = move_cups(many_cups)
-
-    ix = many_cups.index(1)
-    print(f"Part 2: {ix} -> {many_cups[ix:ix + 3]}")
+    cup1, cup2 = ring.subcups(1, 2)
+    print(f"Part 2: {cup1 * cup2}")
