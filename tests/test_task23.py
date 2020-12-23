2cdcfe7455d9b33423230209cece38fee3726814
Carl Skeide <carl@skeide.se>
2020-12-23 16:03:49 +0100

    Fix task 23 tests

--- tests/test_task23.py
+++ tests/test_task23.py
@@ -1,25 +1,39 @@
 # coding=utf-8
 from unittest import TestCase
 
-from src.task23 import move_cups
+from src.task23 import Ring
 
 
-class TestTask(TestCase):
-    def test_move_cups(self):
-        test_input = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+class TestRing(TestCase):
+    def test_move(self):
+        labels = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+
+        ring = Ring(labels)
         self.assertListEqual(
-            move_cups(test_input),
-            [2, 8, 9, 1, 5, 4, 6, 7, 3]
+            list(ring.subcups(3, 8)),
+            [8, 9, 1, 2, 5, 4, 6, 7]
         )
 
-        cups = test_input[:]
+        ring.move()
+        self.assertListEqual(
+            list(ring.subcups(2, 8)),
+            [8, 9, 1, 5, 4, 6, 7, 3]
+        )
+
+        ring = Ring(labels)
         for _ in range(10):
-            cups = move_cups(cups)
+            ring.move()
 
-        self.assertListEqual(cups, [8, 3, 7, 4, 1, 9, 2, 6, 5])
+        self.assertListEqual(
+            list(ring.subcups(8, 8)),
+            [3, 7, 4, 1, 9, 2, 6, 5]
+        )
 
-        cups = test_input[:]
+        ring = Ring(labels)
         for _ in range(100):
-            cups = move_cups(cups)
+            ring.move()
 
-        self.assertListEqual(cups, [1, 6, 7, 3, 8, 4, 5, 2, 9])
+        self.assertListEqual(
+            list(ring.subcups(1, 8)),
+            [6, 7, 3, 8, 4, 5, 2, 9]
+        )
