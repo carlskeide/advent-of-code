287e2a580d2d25454986a7f5ae8ac1e8d6921a83
Carl Skeide <carl@skeide.se>
2020-12-24 23:49:36 +0100

    Task 24, part 2 (cube coords)

--- src/task24.py
+++ src/task24.py
@@ -1,6 +1,14 @@
 # coding=utf-8
 from . import load_input
+
 import re
+from collections import defaultdict
+
+DIRECTIONS = {
+    "nw": (0, -1, 1), "ne": (1, -1, 0),
+    "w": (-1, 0, 1), "e": (1, 0, -1),
+    "sw": (-1, 1, 0), "se": (0, 1, -1)
+}
 
 
 def parse_steps(instructions):
@@ -8,27 +16,48 @@ def parse_steps(instructions):
 
 
 def traverse(floor, steps):
-    directions = {
-        "nw": (-1, 1), "ne": (0, 1),
-        "w": (-1, 0), "e": (1, 0),
-        "sw": (0, -1), "se": (1, -1)
-    }
+    x = y = z = 0
+    for dx, dy, dz in (DIRECTIONS[step] for step in steps):
+        x, y, z = x + dx, y + dy, z + dz
 
-    x, y = 0, 0
-    for dx, dy in (directions[step] for step in steps):
-        x, y = x + dx, y + dy
+    floor[(x, y, z)] = not floor[(x, y, z)]
 
-    floor[(x, y)] = (not floor.get((x, y), 0))
+
+def neighbors(x, y, z):
+    return (((x + dx, y + dy, z + dz) for dx, dy, dz in DIRECTIONS.values()))
+
+
+def flip(floor):
+    for coords in list(floor.keys()):
+        for neighbor in neighbors(*coords):
+            _ = floor[neighbor]
+
+    new_floor = defaultdict(int)
+    for coords in list(floor.keys()):
+        tile = floor[coords]
+        adjacent = sum(floor[neighbor] for neighbor in neighbors(*coords))
+        if tile and (adjacent == 0 or adjacent > 2):
+            new_floor[coords] = 0
+
+        elif not tile and adjacent == 2:
+            new_floor[coords] = 1
+
+        else:
+            new_floor[coords] = floor[coords]
+
+    return new_floor
 
 
 if __name__ == "__main__":
-    task_input = load_input(day=24, group_lines=False)
+    task_input = load_input(day=24)
     charter = [parse_steps(line) for line in task_input]
-    floor = {}
+    floor = defaultdict(int)
     for steps in charter:
         traverse(floor, steps)
 
     print(f"Part 1: {sum(floor.values())}")
 
-    part2 = ""
-    print(f"Part 2: {part2}")
+    for _ in range(100):
+        floor = flip(floor)
+
+    print(f"Part 2: {sum(floor.values())}")
--- tests/test_task24.py
+++ tests/test_task24.py
@@ -1,10 +1,34 @@
 # coding=utf-8
 from unittest import TestCase
 
-from src.task24 import parse_steps, traverse
+from src.task24 import parse_steps, traverse, flip
+from collections import defaultdict
 
 
 class TestTask(TestCase):
+    test_input = [
+        "sesenwnenenewseeswwswswwnenewsewsw",
+        "neeenesenwnwwswnenewnwwsewnenwseswesw",
+        "seswneswswsenwwnwse",
+        "nwnwneseeswswnenewneswwnewseswneseene",
+        "swweswneswnenwsewnwneneseenw",
+        "eesenwseswswnenwswnwnwsewwnwsene",
+        "sewnenenenesenwsewnenwwwse",
+        "wenwwweseeeweswwwnwwe",
+        "wsweesenenewnwwnwsenewsenwwsesesenwne",
+        "neeswseenwwswnwswswnw",
+        "nenwswwsewswnenenewsenwsenwnesesenew",
+        "enewnwewneswsewnwswenweswnenwsenwsw",
+        "sweneswneswneneenwnewenewwneswswnese",
+        "swwesenesewenwneswnwwneseswwne",
+        "enesenwswwswneneswsenwnewswseenwsese",
+        "wnwnesenesenenwwnenwsewesewsesesew",
+        "nenewswnwewswnenesenwnesewesw",
+        "eneswnwswnwsenenwnwnwwseeswneewsenese",
+        "neswnwewnwnwseenwseesewsenwsweewe",
+        "wseweeenwnesenwwwswnew"
+    ]
+
     def test_parse_steps(self):
         self.assertListEqual(
             parse_steps("esenee"), ["e", "se", "ne", "e"])
@@ -13,32 +37,21 @@ class TestTask(TestCase):
             parse_steps("nwwswee"), ["nw", "w", "sw", "e", "e"])
 
     def test_traverse(self):
-        test_input = [
-            "sesenwnenenewseeswwswswwnenewsewsw",
-            "neeenesenwnwwswnenewnwwsewnenwseswesw",
-            "seswneswswsenwwnwse",
-            "nwnwneseeswswnenewneswwnewseswneseene",
-            "swweswneswnenwsewnwneneseenw",
-            "eesenwseswswnenwswnwnwsewwnwsene",
-            "sewnenenenesenwsewnenwwwse",
-            "wenwwweseeeweswwwnwwe",
-            "wsweesenenewnwwnwsenewsenwwsesesenwne",
-            "neeswseenwwswnwswswnw",
-            "nenwswwsewswnenenewsenwsenwnesesenew",
-            "enewnwewneswsewnwswenweswnenwsenwsw",
-            "sweneswneswneneenwnewenewwneswswnese",
-            "swwesenesewenwneswnwwneseswwne",
-            "enesenwswwswneneswsenwnewswseenwsese",
-            "wnwnesenesenenwwnenwsewesewsesesew",
-            "nenewswnwewswnenesenwnesewesw",
-            "eneswnwswnwsenenwnwnwwseeswneewsenese",
-            "neswnwewnwnwseenwseesewsenwsweewe",
-            "wseweeenwnesenwwwswnew"
-        ]
-
-        floor = {}
-        for steps in test_input:
+        floor = defaultdict(int)
+        for steps in self.test_input:
             traverse(floor, parse_steps(steps))
 
         self.assertEqual(len(floor.values()), 15)
         self.assertEqual(sum(floor.values()), 10)
+
+    def test_flip(self):
+        floor = defaultdict(int)
+        for steps in self.test_input:
+            traverse(floor, parse_steps(steps))
+
+        floor = flip(floor)
+        self.assertEqual(sum(floor.values()), 15)
+        floor = flip(floor)
+        self.assertEqual(sum(floor.values()), 12)
+        floor = flip(floor)
+        self.assertEqual(sum(floor.values()), 25)
