d9d1d45c2ec7b8614b65beb089a585849daae7d4
Carl Skeide <carl@skeide.se>
2021-12-01 16:14:37 +0100

    Task 1, part 2

--- 2021/task01.py
+++ 2021/task01.py
@@ -4,6 +4,9 @@ from . import load_input
 def num_increases(depths):
     return sum(depths[i] > depths[i - 1] for i in range(1, len(depths)))
 
+def sliding_increases(depths):
+    groups = [sum(depths[i:i + 3]) for i in range(len(depths) - 2)]
+    return num_increases(groups)
 
 if __name__ == "__main__":
     task_input = load_input(day=1, group_lines=False)
@@ -12,5 +15,5 @@ if __name__ == "__main__":
     part1 = num_increases(depths)
     print(f"Part 1: {part1}")
 
-    part2 = ""
+    part2 = sliding_increases(depths)
     print(f"Part 2: {part2}")
--- 2021/task01_test.py
+++ 2021/task01_test.py
@@ -4,9 +4,12 @@ from unittest import TestCase
 from .task01 import *
 
 class TestTask(TestCase):
-    def test_part1(self):
-        depths = [
-            199, 200, 208, 210, 200, 207, 240, 269, 260, 263
-        ]
+    depths = [
+        199, 200, 208, 210, 200, 207, 240, 269, 260, 263
+    ]
 
-        self.assertEqual(num_increases(depths), 7)
+    def test_part1(self):
+        self.assertEqual(num_increases(self.depths), 7)
+
+    def test_part2(self):
+        self.assertEqual(sliding_increases(self.depths), 5)
