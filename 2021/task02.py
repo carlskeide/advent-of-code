736091985a0e3d56e73560964cdb4fa3669e4ed0
Carl Skeide <carl@skeide.se>
2021-12-02 09:55:27 +0100

    Task 2, part 2

--- 2021/task02.py
+++ 2021/task02.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from . import load_input
 
+
 class Submarine(object):
     def __init__(self):
         super(Submarine, self).__init__()
@@ -21,6 +22,27 @@ class Submarine(object):
                 self.depth -= int(value)
 
 
+class DeltaSubmarine(Submarine):
+    def __init__(self):
+        super(DeltaSubmarine, self).__init__()
+        self.aim = 0
+
+    def run(self, instructions):
+        for instruction in instructions:
+            command, value = instruction.split()
+
+            if command == "forward":
+                delta = int(value)
+                self.distance += delta
+                self.depth += self.aim * delta
+
+            elif command == "down":
+                self.aim += int(value)
+
+            elif command == "up":
+                self.aim -= int(value)
+
+
 if __name__ == "__main__":
     instructions = load_input(day=2, group_lines=False)
 
@@ -30,5 +52,8 @@ if __name__ == "__main__":
     part1 = submarine.distance * submarine.depth
     print(f"Part 1: {part1}")
 
-    part2 = ""
+    delta_submarine = DeltaSubmarine()
+    delta_submarine.run(instructions)
+
+    part2 = delta_submarine.distance * delta_submarine.depth
     print(f"Part 2: {part2}")
--- 2021/task02_test.py
+++ 2021/task02_test.py
@@ -1,22 +1,29 @@
 # coding=utf-8
 from unittest import TestCase
 
-from .task02 import Submarine
+from .task02 import Submarine, DeltaSubmarine
 
 
 class TestTask(TestCase):
-    def test_part1(self):
-        instructions = [
-            "forward 5",
-            "down 5",
-            "forward 8",
-            "up 3",
-            "down 8",
-            "forward 2"
-        ]
+    instructions = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2"
+    ]
 
+    def test_part1(self):
         submarine = Submarine()
-        submarine.run(instructions)
+        submarine.run(self.instructions)
 
         self.assertEqual(submarine.distance, 15)
         self.assertEqual(submarine.depth, 10)
+
+    def test_part2(self):
+        submarine = DeltaSubmarine()
+        submarine.run(self.instructions)
+
+        self.assertEqual(submarine.distance, 15)
+        self.assertEqual(submarine.depth, 60)
