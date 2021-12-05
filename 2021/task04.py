bf237628df01de34c9cd800b349273a30f61ac16
Carl Skeide <carl@skeide.se>
2021-12-05 01:33:58 +0100

    Task 4, OO refactor

--- 2021/task04.py
+++ 2021/task04.py
@@ -2,64 +2,65 @@
 from . import load_input
 
 
-def parse_boards(charter):
-    return [
-        [row.split() for row in board.splitlines()]
-        for board in charter
-    ]
+class BingoBoard():
+    def __init__(self, board, size=5):
+        self.size = size
+        self.rows = [
+            [int(col) for col in row.split()]
+            for row in board.splitlines()
+        ]
 
+    @property
+    def cols(self):
+        return [
+            [self.rows[y][x] for y in range(self.size)]
+            for x in range(self.size)
+        ]
 
-def has_bingo(board):
-    cols = [
-        [board[y][x] for y in range(5)]
-        for x in range(5)
-    ]
+    @property
+    def bingo(self):
+        return any(
+            all(c is None for c in line)
+            for line in self.rows + self.cols
+        )
 
-    return any(
-        all(c == "" for c in row)
-        for row in board + cols
-    )
+    @property
+    def value(self):
+        return sum(
+            sum(filter(None, row))
+            for row in self.rows
+        )
 
+    def call(self, number):
+        for row in self.rows:
+            if number in row:
+                row[row.index(number)] = None
 
-def score_board(board, number):
-    return [
-        ["" if c == number else c for c in row]
-        for row in board
-    ]
-
-
-def board_value(board):
-    return sum(
-        sum(int(col) for col in row if col)
-        for row in board
-    )
 
 
 def play_bingo(boards, numbers, win=True):
-    bingo_boards = []
     for number in numbers:
         print(f"calling number {number}")
 
-        boards = [score_board(board, number) for board in boards]
-        for i, board in enumerate(boards):
-            if has_bingo(board) and i not in bingo_boards:
-                print(f"BINGO! board {i}")
-                bingo_boards.append(i)
+        boards = [board for board in boards if not board.bingo]
+        for board in boards:
+            board.call(number)
+            if board.bingo:
+                print(f"BINGO!")
 
-                if win or len(bingo_boards) == len(boards):
-                    print("Game over")
+                if win or len(boards) == 1:
                     return board, number
 
 
 if __name__ == "__main__":
     task_input = load_input(day=4, group_lines=True)
-    numbers = task_input[0].split(',')
-    boards = parse_boards(task_input[1:])
+    numbers = list(map(int, task_input[0].split(',')))
 
+    boards = list(map(BingoBoard, task_input[1:]))
     board, number = play_bingo(boards, numbers)
-    part1 = board_value(board) * int(number)
+    part1 = board.value * number
     print(f"Part 1: {part1}")
 
     board, number = play_bingo(boards, numbers, win=False)
-    part2 = board_value(board) * int(number)
+    part2 = board.value * number
     print(f"Part 2: {part2}")
--- 2021/task04_test.py
+++ 2021/task04_test.py
@@ -3,106 +3,110 @@ from unittest import TestCase
 
 from .task04 import *
 
-TEST_NUMBERS = (
-    "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,"
-    "6,15,25,12,22,18,20,8,19,3,26,1".split(",")
-)
+TEST_NUMBERS = list(map(int,
+    "7,4,9,5,11,17,23,2,0,14,21,24,10,16,"
+    "13,6,15,25,12,22,18,20,8,19,3,26,1".split(",")
+))
 
 TEST_BOARDS = [
-    [
-        ['22', '13', '17', '11', '0'],
-        ['8', '2', '23', '4', '24'],
-        ['21', '9', '14', '16', '7'],
-        ['6', '10', '3', '18', '5'],
-        ['1', '12', '20', '15', '19']
-    ],
-    [
-        ['3', '15', '0', '2', '22'],
-        ['9', '18', '13', '17', '5'],
-        ['19', '8', '7', '25', '23'],
-        ['20', '11', '10', '24', '4'],
-        ['14', '21', '16', '12', '6']
-    ],
-    [
-        ['14', '21', '17', '24', '4'],
-        ['10', '16', '15', '9', '19'],
-        ['18', '8', '23', '26', '20'],
-        ['22', '11', '13', '6', '5'],
-        ['2', '0', '12', '3', '7']
-    ]
+    "22 13 17 11  0\n"
+    " 8  2 23  4 24\n"
+    "21  9 14 16  7\n"
+    " 6 10  3 18  5\n"
+    " 1 12 20 15 19\n",
+
+    " 3 15  0  2 22\n"
+    " 9 18 13 17  5\n"
+    "19  8  7 25 23\n"
+    "20 11 10 24  4\n"
+    "14 21 16 12  6\n",
+
+    "14 21 17 24  4\n"
+    "10 16 15  9 19\n"
+    "18  8 23 26 20\n"
+    "22 11 13  6  5\n"
+    " 2  0 12  3  7\n"
 ]
 
 BINGO_BOARD = [
-    ['', '', '', '', ''],
-    ['10', '16', '15', '', '19'],
-    ['18', '8', '', '26', '20'],
-    ['22', '', '13', '6', ''],
-    ['', '', '12', '3', '']
+    [None, None, None, None, None],
+    [10, 16, 15, None, 19],
+    [18, 8, None, 26, 20],
+    [22, None, 13, 6, None],
+    [None, None, 12, 3, None]
 ]
 
 LAST_BOARD = [
-    ['3', '15', '', '', '22'],
-    ['', '18', '', '', ''],
-    ['19', '8', '', '25', ''],
-    ['20', '', '', '', ''],
-    ['', '', '', '12', '6']
+    [3, 15, None, None, 22],
+    [None, 18, None, None, None],
+    [19, 8, None, 25, None],
+    [20, None, None, None, None],
+    [None, None, None, 12, 6]
 ]
 
 
-class TestTask(TestCase):
-    def test_parse_boards(self):
-        board_input = [
-            "22 13 17 11  0\n"
-            " 8  2 23  4 24\n"
-            "21  9 14 16  7\n"
-            " 6 10  3 18  5\n"
-            " 1 12 20 15 19\n"
-        ]
+class TestBingoBoard(TestCase):
+    def setUp(self):
+        self.board = BingoBoard(TEST_BOARDS[0])
 
+    def test_init(self):
         self.assertListEqual(
-            parse_boards(board_input)[0][0],
-            ["22", "13", "17", "11", "0"]
+            self.board.rows,
+            [
+                [22, 13, 17, 11, 0],
+                [8, 2, 23, 4, 24],
+                [21, 9, 14, 16, 7],
+                [6, 10, 3, 18, 5],
+                [1, 12, 20, 15, 19]
+            ]
         )
 
     def test_has_bingo(self):
-        self.assertFalse(has_bingo(TEST_BOARDS[0]))
-        self.assertTrue(has_bingo(BINGO_BOARD))
-        self.assertTrue(has_bingo(
-            [['14', '21', '17', '24', ''],
-            ['10', '16', '15', '', ''],
-            ['18', '8', '', '26', ''],
-            ['22', '', '13', '6', ''],
-            ['', '', '12', '3', '']]
-        ))
+        self.assertFalse(self.board.bingo)
 
-    def test_score_board(self):
-        self.assertEqual(
-            score_board(
-                [['14', '21', '17', '24', '4'],
-                ['10', '16', '15', '9', '19'],
-                ['18', '8', '23', '26', '20'],
-                ['22', '11', '13', '6', '5'],
-                ['2', '0', '12', '3', '7']],
-                '18'
-            ),
-            [['14', '21', '17', '24', '4'],
-            ['10', '16', '15', '9', '19'],
-            ['', '8', '23', '26', '20'],
-            ['22', '11', '13', '6', '5'],
-            ['2', '0', '12', '3', '7']],
+        self.board.rows = BINGO_BOARD
+        self.assertTrue(self.board.bingo)
+
+        self.board.rows = LAST_BOARD
+        self.assertTrue(self.board.bingo)
+
+    def test_call(self):
+        self.board.call(18)
+        self.board.call(2)
+        self.board.call(25)
+
+        self.assertListEqual(
+            self.board.rows,
+            [
+                [22, 13, 17, 11, 0],
+                [8, None, 23, 4, 24],
+                [21, 9, 14, 16, 7],
+                [6, 10, 3, None, 5],
+                [1, 12, 20, 15, 19]
+            ]
         )
 
-    def test_board_value(self):
-        self.assertEqual(board_value(BINGO_BOARD), 188)
-        self.assertEqual(board_value(LAST_BOARD), 148)
+    def test_value(self):
+        self.board.rows = BINGO_BOARD
+        self.assertEqual(self.board.value, 188)
+
+        self.board.rows = LAST_BOARD
+        self.assertEqual(self.board.value, 148)
+
+
+class TestTask(TestCase):
+    def setUp(self):
+        self.boards = list(map(BingoBoard, TEST_BOARDS))
 
     def test_part1(self):
-        board, number = play_bingo(TEST_BOARDS, TEST_NUMBERS)
-        self.assertEqual(board, BINGO_BOARD)
-        self.assertEqual(number, "24")
+        board, number = play_bingo(self.boards, TEST_NUMBERS)
+
+        self.assertEqual(number, 24)
+        self.assertEqual(board.rows, BINGO_BOARD)
 
 
     def test_part2(self):
-        board, number = play_bingo(TEST_BOARDS, TEST_NUMBERS, win=False)
-        self.assertEqual(number, "13")
-        self.assertEqual(board, LAST_BOARD)
+        board, number = play_bingo(self.boards, TEST_NUMBERS, win=False)
+
+        self.assertEqual(number, 13)
+        self.assertEqual(board.rows, LAST_BOARD)
