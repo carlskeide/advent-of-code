7aabfe51e2e144b6b22ff10124ba40767f64c695
Carl Skeide <carl.skeide@sony.com>
2021-12-06 16:18:27 +0100

    Task 6, part 2

--- 2021/task06.py
+++ 2021/task06.py
@@ -1,20 +1,27 @@
 # coding=utf-8
 from . import load_input
+from collections import Counter
 
 
 def simulate(fishes, days):
-    for d in range(days):
-        fishes = [i - 1 if i > 0 else 6 for i in fishes] + ([8] * fishes.count(0))
+    school = Counter(fishes)
+    for _ in range(days):
+        spawm = school.get(0, 0)
+        for i in range(8):
+            school[i] = school.get(i + 1, 0)
 
-    return fishes
+        school[6] += spawm
+        school[8] = spawm
+
+    return sum(school.values())
 
 
 if __name__ == "__main__":
     task_input = load_input(day=6, group_lines=False)
-    school = list(map(int, task_input[0].split(",")))
+    school = [int(s) for s in task_input[0].split(",")]
 
-    part1 = len(simulate(school, days=80))
+    part1 = simulate(school, days=80)
     print(f"Part 1: {part1}")
 
-    # part2 = len(simulate(school, days=256))
-    # print(f"Part 2: {part2}")
+    part2 = simulate(school, days=256)
+    print(f"Part 2: {part2}")
