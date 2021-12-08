1306988ea1d5458d0a9a021fffbc578913952adc
Carl Skeide <carl.skeide@sony.com>
2021-12-08 15:37:41 +0100

    Task 8, part 2

--- 2021/task08.py
+++ 2021/task08.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 from itertools import chain
-from collections import Counter
 from . import load_input
 
 SIMPLE = {
@@ -15,16 +14,48 @@ def infer_basic(segments):
     return (SIMPLE.get(len(segment), None) for segment in segments)
 
 
+def find_key(patterns):
+    parsed = [(len(pattern), frozenset(pattern)) for pattern in patterns]
+    key = {SIMPLE[plen]: pset for plen, pset in parsed if plen in SIMPLE}
+    for plen, pset in parsed:
+        if plen == 5:
+            if pset.issuperset(key[1]):
+                key[3] = pset
+
+            elif pset.issuperset(key[4] - key[1]):
+                key[5] = pset
+
+            else:
+                key[2] = pset
+
+        elif plen == 6:
+            if not pset.issuperset(key[1]):
+                key[6] = pset
+
+            elif pset.issuperset(key[4]):
+                key[9] = pset
+
+            else:
+                key[0] = pset
+
+    return key
+
+
+def decode(key, patterns):
+    revkey = {v: k for k, v in key.items()}
+    return int("".join(str(revkey[frozenset(s)]) for s in patterns))
+
+
 if __name__ == "__main__":
     task_input = load_input(day=8, group_lines=False)
     segments = [
-        (part.split() for part in line.split(' | '))
+        tuple(part.split() for part in line.split(' | '))
         for line in task_input
     ]
 
-    inferred = chain.from_iterable(infer_basic(s) for _, s in segments)
+    inferred = chain.from_iterable(infer_basic(out) for _, out in segments)
     part1 = len(list(filter(None, inferred)))
     print(f"Part 1: {part1}")
 
-    part2 = ""
+    part2 = sum(decode(find_key(patterns), out) for patterns, out in segments)
     print(f"Part 2: {part2}")
--- 2021/task08_test.py
+++ 2021/task08_test.py
@@ -15,3 +15,25 @@ class TestTask(TestCase):
             list(infer_basic(("fdgacbe", "cefdb", "cefbgd", "gcbe"))),
             [8, None, None, 4]
         )
+
+    def test_find_key(self):
+        key = find_key([
+            "acedgfb", "cdfbe", "gcdfa", "fbcad", "dab",
+            "cefabd", "cdfgeb", "eafb", "cagedb", "ab"
+        ])
+
+        self.assertSetEqual(key[5], set("cdfeb"))
+        self.assertSetEqual(key[3], set("fcadb"))
+        self.assertSetEqual(key[2], set("gcdfa"))
+
+        self.assertSetEqual(key[9], set("cefabd"))
+        self.assertSetEqual(key[6], set("cdfgeb"))
+        self.assertSetEqual(key[0], set("cagedb"))
+
+    def test_decode(self):
+        key = find_key([
+            "be", "cfbegad", "cbdgef", "fgaecd", "cgeb",
+            "fdcge", "agebfd", "fecdb", "fabcd", "edb"
+        ])
+        self.assertEqual(
+            decode(key, ("fdgacbe", "cefdb", "cefbgd", "gcbe")), 8394)
